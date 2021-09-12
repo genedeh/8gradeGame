57e3c5c5287019d4dd878e61640602170d66a28c
KidChaos <genesis.enedeh@gmail.com>
2021-09-12 22:04:41 +0100

    made key private

--- password_maker.py
+++ password_maker.py
@@ -6,21 +6,21 @@ class PasswordMaker:
     def __init__(self, key_words, code_name="Code Name", key=6):
         self.__key_words = key_words
         self.__code_name = code_name
-        self.key = key
+        self.__key = key
 
     def DigitPassword(self):
-        return "".join(random.choices(string.digits, k=self.key))
+        return "".join(random.choices(string.digits, k=self.__key))
 
     def WordPassword(self):
-        return "".join(random.choices(self.__key_words + string.ascii_letters, k=self.key))
+        return "".join(random.choices(self.__key_words + string.ascii_letters, k=self.__key))
 
     def CodePassword(self):
-        return "".join(random.choices(self.__code_name + string.digits + string.ascii_letters, k=self.key))
+        return "".join(random.choices(self.__code_name + string.digits + string.ascii_letters, k=self.__key))
 
     def RandomPassword(self):
         return "".join(
-            random.choices(self.__key_words + self.__code_name + string.digits + string.ascii_letters, k=self.key))
+            random.choices(self.__key_words + self.__code_name + string.digits + string.ascii_letters, k=self.__key))
 
 
-passcode_bot = PasswordMaker("Genesis", code_name="bro force one", key=6)
+passcode_bot = PasswordMaker("Genesis", code_name="bro force one", key=8)
 print(passcode_bot.CodePassword())
