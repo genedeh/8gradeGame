40696185d6b44b602f56a582672001a913530036
KidChaos <genesis.enedeh@gmail.com>
2021-09-12 21:24:08 +0100

    made my methods more defensive using try and except ValueError

--- 8GradeGame.py
+++ 8GradeGame.py
@@ -3,7 +3,13 @@ import string
 
 
 class MathsGame:
-    def __init__(self, profile_name, name, seconds=20):
+    """ This is  a maths game that lets the user to check his or her skills in
+       addition, subtraction, division, multiplication and addition plus subtraction
+       :parameter: profile_name: str, name: str, seconds: int
+       SAMPLE:
+    """
+
+    def __init__(self, profile_name: str, name: str, seconds: int = 20):
         self.profile_name = profile_name
         self.name = name
         self.seconds = seconds
@@ -11,94 +17,115 @@ class MathsGame:
     def __str__(self):
         return f"name: {self.name}, profile name: {self.profile_name}"
 
-    def Addition(self):
+    def addition(self):
+        global answer
         while True:
-            easy_random1 = int(random.choice(string.digits))
-            easy_random2 = int(random.choice(string.digits))
-            easy_random3 = int(random.choice(string.digits))
-            easy_random4 = int(random.choice(string.digits))
-            print(f"{easy_random1} + {easy_random2} + {easy_random3} + {easy_random4} = ?")
-            real_answer = easy_random1 + easy_random2 + easy_random3 + easy_random4
-            answer = input("Enter answer: ")
-            if answer.lower() == "stop":
-                print("okay")
-                break
-            if int(answer) == real_answer:
-                print("CORRECT ANSWER")
-            else:
-                print("WRONG ANSWER")
-                print(f"the answer is {real_answer} sorry! try again")
+            try:
+                easy_random1 = int(random.choice(string.digits))
+                easy_random2 = int(random.choice(string.digits))
+                easy_random3 = int(random.choice(string.digits))
+                easy_random4 = int(random.choice(string.digits))
+                print(f"{easy_random1} + {easy_random2} + {easy_random3} + {easy_random4} = ?")
+                real_answer = easy_random1 + easy_random2 + easy_random3 + easy_random4
+                answer = input("Enter answer: ")
+                if answer.lower() == "stop":
+                    print("okay")
+                    break
+                if int(answer) == real_answer:
+                    print("CORRECT ANSWER")
+                else:
+                    print("WRONG ANSWER")
+                    print(f"the answer is {real_answer} sorry! try again")
+            except ValueError:
+                return f'"{answer}" is not a valid number, only the string stop is allowed'
 
-    def Subtraction(self):
+    def subtraction(self):
+        global answer
         while True:
-            easy_random1 = int(random.choice(string.digits))
-            easy_random2 = int(random.choice(string.digits))
-            easy_random3 = int(random.choice(string.digits))
-            easy_random4 = int(random.choice(string.digits))
-            print(f"{easy_random1} - {easy_random2} - {easy_random3} - {easy_random4} = ?")
-            real_answer = easy_random1 - easy_random2 - easy_random3 - easy_random4
-            answer = input("Enter answer: ")
-            if answer.lower() == "stop":
-                print("okay")
-                break
-            if int(answer) == real_answer:
-                print("CORRECT ANSWER")
-            else:
-                print("WRONG ANSWER")
-                print(f"the answer is {real_answer} sorry! try again")
+            try:
+                easy_random1 = int(random.choice(string.digits))
+                easy_random2 = int(random.choice(string.digits))
+                easy_random3 = int(random.choice(string.digits))
+                easy_random4 = int(random.choice(string.digits))
+                print(f"{easy_random1} - {easy_random2} - {easy_random3} - {easy_random4} = ?")
+                real_answer = easy_random1 - easy_random2 - easy_random3 - easy_random4
+                answer = input("Enter answer: ")
+                if answer.lower() == "stop":
+                    print("okay")
+                    break
+                if int(answer) == real_answer:
+                    print("CORRECT ANSWER")
+                else:
+                    print("WRONG ANSWER")
+                    print(f"the answer is {real_answer} sorry! try again")
+            except ValueError:
+                return f'"{answer}" is not a valid number, only the string stop is allowed'
 
-    def Division(self):
+    def division(self):
+        global answer
         while True:
-            easy_random1 = int(random.choice(string.digits))
-            easy_random2 = int(random.choice(string.digits))
-            easy_random3 = int(random.choice(string.digits))
-            easy_random4 = int(random.choice(string.digits))
-            print(f"{easy_random1} / {easy_random2} / {easy_random3} / {easy_random4} = ?")
-            real_answer = easy_random1 / easy_random2 / easy_random3 / easy_random4
-            answer = input("Enter answer: ")
-            if answer.lower() == "stop":
-                print("okay")
-                break
-            if float(answer) == real_answer:
-                print("CORRECT ANSWER")
-            else:
-                print("WRONG ANSWER")
-                print(f"the answer is {real_answer} sorry! try again")
-    def Multiplication(self):
-        while True:
-            easy_random1 = int(random.choice(string.digits))
-            easy_random2 = int(random.choice(string.digits))
-            easy_random3 = int(random.choice(string.digits))
-            easy_random4 = int(random.choice(string.digits))
-            print(f"{easy_random1} * {easy_random2} * {easy_random3} * {easy_random4} = ?")
-            real_answer = easy_random1 * easy_random2 * easy_random3 * easy_random4
-            answer = input("Enter answer: ")
-            if answer.lower() == "stop":
-                print("okay")
-            if int(answer) == real_answer:
-                print("CORRECT ANSWER")
-            else:
-                print("WRONG ANSWER")
-                print(f"the answer is {real_answer} sorry! try again")
+            try:
+                easy_random1 = int(random.choice(string.digits))
+                easy_random2 = int(random.choice(string.digits))
+                easy_random3 = int(random.choice(string.digits))
+                easy_random4 = int(random.choice(string.digits))
+                print(f"{easy_random1} / {easy_random2} / {easy_random3} / {easy_random4} = ?")
+                real_answer = easy_random1 / easy_random2 / easy_random3 / easy_random4
+                answer = input("Enter answer: ")
+                if answer.lower() == "stop":
+                    print("okay")
+                    break
+                if float(answer) == real_answer:
+                    print("CORRECT ANSWER")
+                else:
+                    print("WRONG ANSWER")
+                    print(f"the answer is {real_answer} sorry! try again")
+            except ValueError:
+                return f'"{answer}" is not a valid number, only the string stop is allowed'
 
-    def Addition_Subtraction(self):
+    def multiplication(self):
+        global answer
         while True:
-            easy_random1 = int(random.choice(string.digits))
-            easy_random2 = int(random.choice(string.digits))
-            easy_random3 = int(random.choice(string.digits))
-            easy_random4 = int(random.choice(string.digits))
-            print(f"{easy_random1} / {easy_random2} / {easy_random3} / {easy_random4} = ?")
-            real_answer = easy_random1 + easy_random2 - easy_random3 + easy_random4
-            answer = input("Enter answer: ")
-            if answer.lower() == "stop":
-                print("okay")
-                break
-            if int(answer) == real_answer:
-                print("CORRECT ANSWER")
-            else:
-                print("WRONG ANSWER")
-                print(f"the answer is {real_answer} sorry! try again")
+            try:
+                easy_random1 = int(random.choice(string.digits))
+                easy_random2 = int(random.choice(string.digits))
+                easy_random3 = int(random.choice(string.digits))
+                easy_random4 = int(random.choice(string.digits))
+                print(f"{easy_random1} * {easy_random2} * {easy_random3} * {easy_random4} = ?")
+                real_answer = easy_random1 * easy_random2 * easy_random3 * easy_random4
+                answer = input("Enter answer: ")
+                if answer.lower() == "stop":
+                    print("okay")
+                if int(answer) == real_answer:
+                    print("CORRECT ANSWER")
+                else:
+                    print("WRONG ANSWER")
+                    print(f"the answer is {real_answer} sorry! try again")
+            except ValueError:
+                return f'"{answer}" is not a valid number, only the string stop is allowed'
+
+    def addition_subtraction(self):
+        global answer
+        while True:
+            try:
+                easy_random1 = int(random.choice(string.digits))
+                easy_random2 = int(random.choice(string.digits))
+                easy_random3 = int(random.choice(string.digits))
+                easy_random4 = int(random.choice(string.digits))
+                print(f"{easy_random1} + {easy_random2} - {easy_random3} + {easy_random4} = ?")
+                real_answer = easy_random1 + easy_random2 - easy_random3 + easy_random4
+                answer = input("Enter answer: ")
+                if answer.lower() == "stop":
+                    print("okay")
+                    break
+                if int(answer) == real_answer:
+                    print("CORRECT ANSWER")
+                else:
+                    print("WRONG ANSWER")
+                    print(f"the answer is {real_answer} sorry! try again")
+            except ValueError:
+                return f'"{answer}" is not a valid number, only the string stop is allowed'
 
 
 maths_game = MathsGame("testing", "test")
-print(maths_game.Addition())
+print(maths_game.addition_subtraction())
